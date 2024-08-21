217787c12e4b7a405d47aee2e3da53133744e798
Dev-Y0U <dev.yousef.waleed@gmail.com>
2024-08-21 04:52:09 +0300

    improve the createtion tables

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include "sqlite3.h"
 #include <cstdlib>
+#include<map>
 
 using namespace std;
 
@@ -50,115 +51,29 @@ private:
     string query;
     connection db;
     int response = db.connect();
+    map<string,string> tableQueries = {
+        {"Patients", "CREATE TABLE IF NOT EXISTS Patients (PatientID INTEGER PRIMARY KEY AUTOINCREMENT, Name VARCHAR(255) NOT NULL, BirthDate DATE NOT NULL, Gender CHAR(1) NOT NULL, ContactNumber VARCHAR(15), Email VARCHAR(255), Address TEXT);"},
+        {"Doctors", "CREATE TABLE IF NOT EXISTS Doctors (DoctorID INTEGER PRIMARY KEY AUTOINCREMENT, Name VARCHAR(255) NOT NULL, BirthDate DATE NOT NULL, Gender CHAR(1) NOT NULL, Specialization VARCHAR(255), ContactNumber VARCHAR(15), Email VARCHAR(255), Address TEXT);"},
+        {"Appointments", "CREATE TABLE IF NOT EXISTS Appointments (AppointmentID INTEGER PRIMARY KEY AUTOINCREMENT, PatientID INT NOT NULL, DoctorID INT NOT NULL, AppointmentDate DATETIME NOT NULL, Status VARCHAR(50), CancellationReason TEXT, RescheduledDate DATETIME, Notes TEXT, FOREIGN KEY (PatientID) REFERENCES Patients (PatientID), FOREIGN KEY (DoctorID) REFERENCES Doctors (DoctorID));"},
+        {"MedicalRecords", "CREATE TABLE IF NOT EXISTS MedicalRecords (RecordID INTEGER PRIMARY KEY AUTOINCREMENT, PatientID INT NOT NULL, AppointmentID INT NOT NULL, Diagnosis TEXT, Prescription TEXT, Notes TEXT, FOREIGN KEY (PatientID) REFERENCES Patients (PatientID), FOREIGN KEY (AppointmentID) REFERENCES Appointments (AppointmentID));"},
+        {"Prescriptions", "CREATE TABLE IF NOT EXISTS Prescriptions (PrescriptionID INTEGER PRIMARY KEY AUTOINCREMENT, RecordID INT NOT NULL, DrugName VARCHAR(255) NOT NULL, Dosage VARCHAR(50), Frequency VARCHAR(50), StartDate DATE, EndDate DATE, SpecialInstructions TEXT, FOREIGN KEY (RecordID) REFERENCES MedicalRecords (RecordID));"},
+        {"Payments", "CREATE TABLE IF NOT EXISTS Payments (PaymentID INTEGER PRIMARY KEY AUTOINCREMENT, PatientID INT NOT NULL, AppointmentID INT NOT NULL, PaymentDate DATETIME NOT NULL, Amount DECIMAL(10, 2), PaymentMethod VARCHAR(50), Notes TEXT, FOREIGN KEY (PatientID) REFERENCES Patients (PatientID), FOREIGN KEY (AppointmentID) REFERENCES Appointments (AppointmentID));"}
+    };
+
 
 public:
     void createDb()
     {
+           for (auto& entry : tableQueries) {
+           
 
-        query = "CREATE TABLE IF NOT EXISTS Patients ("
-              "PatientID INTEGER PRIMARY KEY AUTOINCREMENT, "
-              "Name VARCHAR(255) NOT NULL, "
-              "BirthDate DATE NOT NULL, "
-              "Gender CHAR(1) NOT NULL, "
-              "ContactNumber VARCHAR(15), "
-              "Email VARCHAR(255), "
-              "Address TEXT);";
-
-        response = sqlite3_exec(DB, query.c_str(), NULL, 0, &errMsg);
+        response = sqlite3_exec(DB, entry.second.c_str(), NULL, 0, &errMsg);
         if (response != SQLITE_OK)
         {
-            cerr << "Error Create Patients Table: " << errMsg << endl;
+            cerr << "Error Create "+ entry.first +" Table: " << errMsg << endl;
             sqlite3_free(errMsg);
         }
-
-        query = "CREATE TABLE IF NOT EXISTS Doctors ("
-              "DoctorID INTEGER PRIMARY KEY AUTOINCREMENT, "
-              "Name VARCHAR(255) NOT NULL, "
-              "BirthDate DATE NOT NULL, "
-              "Gender CHAR(1) NOT NULL, "
-              "Specialization VARCHAR(255), "
-              "ContactNumber VARCHAR(15), "
-              "Email VARCHAR(255), "
-              "Address TEXT);";
-
-        response = sqlite3_exec(DB, query.c_str(), NULL, 0, &errMsg);
-        if (response != SQLITE_OK)
-        {
-            cerr << "Error Create Doctors Table: " << errMsg << endl;
-            sqlite3_free(errMsg);
-        }
-
-        query = "CREATE TABLE IF NOT EXISTS Appointments ("
-              "AppointmentID INTEGER PRIMARY KEY AUTOINCREMENT, "
-              "PatientID INT NOT NULL, "
-              "DoctorID INT NOT NULL, "
-              "AppointmentDate DATETIME NOT NULL, "
-              "Status VARCHAR(50), "
-              "CancellationReason TEXT, "
-              "RescheduledDate DATETIME, "
-              "Notes TEXT, "
-              "FOREIGN KEY(PatientID) REFERENCES Patients(PatientID), "
-              "FOREIGN KEY(DoctorID) REFERENCES Doctors(DoctorID));";
-
-        response = sqlite3_exec(DB, query.c_str(), NULL, 0, &errMsg);
-        if (response != SQLITE_OK)
-        {
-            cerr << "Error Create Appointments Table: " << errMsg << endl;
-            sqlite3_free(errMsg);
-        }
-
-        query = "CREATE TABLE IF NOT EXISTS MedicalRecords ("
-              "RecordID INTEGER PRIMARY KEY AUTOINCREMENT, "
-              "PatientID INT NOT NULL, "
-              "AppointmentID INT NOT NULL, "
-              "Diagnosis TEXT, "
-              "Prescription TEXT, "
-              "Notes TEXT, "
-              "FOREIGN KEY(PatientID) REFERENCES Patients(PatientID), "
-              "FOREIGN KEY(AppointmentID) REFERENCES Appointments(AppointmentID));";
-
-        response = sqlite3_exec(DB, query.c_str(), NULL, 0, &errMsg);
-        if (response != SQLITE_OK)
-        {
-            cerr << "Error Create MedicalRecords Table: " << errMsg << endl;
-            sqlite3_free(errMsg);
-        }
-
-        query = "CREATE TABLE IF NOT EXISTS Prescriptions ("
-              "PrescriptionID INTEGER PRIMARY KEY AUTOINCREMENT, "
-              "RecordID INT NOT NULL, "
-              "DrugName VARCHAR(255) NOT NULL, "
-              "Dosage VARCHAR(50), "
-              "Frequency VARCHAR(50), "
-              "StartDate DATE, "
-              "EndDate DATE, "
-              "SpecialInstructions TEXT, "
-              "FOREIGN KEY(RecordID) REFERENCES MedicalRecords(RecordID));";
-
-        response = sqlite3_exec(DB, query.c_str(), NULL, 0, &errMsg);
-        if (response != SQLITE_OK)
-        {
-            cerr << "Error Create Prescriptions Table: " << errMsg << endl;
-            sqlite3_free(errMsg);
-        }
-
-        query = "CREATE TABLE IF NOT EXISTS Payments ("
-              "PaymentID INTEGER PRIMARY KEY AUTOINCREMENT, "
-              "PatientID INT NOT NULL, "
-              "AppointmentID INT NOT NULL, "
-              "PaymentDate DATETIME NOT NULL, "
-              "Amount DECIMAL(10,2), "
-              "PaymentMethod VARCHAR(50), "
-              "Notes TEXT, "
-              "FOREIGN KEY(PatientID) REFERENCES Patients(PatientID), "
-              "FOREIGN KEY(AppointmentID) REFERENCES Appointments(AppointmentID));";
-
-        response = sqlite3_exec(DB, query.c_str(), NULL, 0, &errMsg);
-        if (response != SQLITE_OK)
-        {
-            cerr << "Error Create Payments Table: " << errMsg << endl;
-            sqlite3_free(errMsg);
-        }
+           }
 
         db.disoncect();
     }
@@ -856,7 +771,7 @@ void handle()
     while (true)
     {
         clearScreen();
-        menu();
+        main_menu();
         cin >> choice;
 
         switch (choice)
